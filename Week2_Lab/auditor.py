6254c945eed0dfc881f7a8395d8abe1848f09d73
Jyoshna <2604709@sit.singaporetech.edu.sg>
2026-09-13 00:03:20 +0800

    Created variable to collect input from user abt stock quntity, whether to continue asking for input and error message for when entered input is not an integer

---
+++ Week2_Lab/auditor.py
@@ -0,0 +1,13 @@
+inventory = 0
+failed_entries = 0
+
+while True:
+    stock = input("Enter stock quantity (or type 'quite' to stop): ")
+
+    if stock.lower() == 'quit':
+        break
+
+        if not stock.isdigit():
+            print("Please enter a valid integer")
+            failed_entries +=1
+            comntinue
