091b4769e708bfa2fabd003af74f6bb653d14bd1
Jyoshna <2604709@sit.singaporetech.edu.sg>
2026-09-18 09:46:11 +0800

    modified get valid input function to properly return values and focus on requirements needed within this function

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,58 +1,30 @@
 inventory = 0
 failed_entries = 0
 
-# while True:
-#     stock = input("Enter stock quantity (or type 'quit' to stop): ")
 
-#     if stock.lower() == 'quit':
-#         break
+def get_valid_input():
 
-#     if not stock.isdigit():
-#             print("Please enter a valid integer")
-#             failed_entries +=1
-#             continue
-    
-#     stock = int(stock)
-   
-#     if stock < 0:
-#         print("Negative numbers are not allowed")
-#         failed_entries += 1
-#         continue
+    stock = input("Enter stock quantity (or type 'quit' to stop): ")
 
-    
-#     inventory += stock
+    if stock.lower() == 'quit':
+        return "quit"
 
-#     if inventory > 500:
-#          print("Inventory exceeds 500 units")
-#          break
+    if not stock.isdigit():
+        print("Please enter a valid integer")
+        return None
+
+    stock = int(stock)
+
+    if stock < 0:
+        print("Negative numbers are not allowed")
+        return None
+
+    return stock
 
 
-# print("Total Units Processed: ", inventory)
-# print("Number of failed/ rejected entries: ", failed_entries)
-    
 
-def get_valid_input(inventory, failed_entries):
 
-    while True:
 
-        stock = input("Enter stock quantity (or type 'quit' to stop): ")
 
-        if stock.lower() == 'quit':
-            break
 
-        if not stock.isdigit():
-            print("Please enter a valid integer")
-            failed_entries +=1
-            continue
-
-        stock = int(stock)
-
-        if stock < 0:
-            print("Negative numbers are not allowed")
-            failed_entries += 1
-            continue
-
-    return stock, failed_entries
-
-get_valid_input(inventory, failed_entries)
-
+get_valid_input()
